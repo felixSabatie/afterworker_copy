3075a53e6d268299220337d47853a8de3a4374f5
Unknown <felix.sabatie@gmail.com>
2019-02-08 18:54:41 -0500

    Add uniqueness to pseudo and email

--- db/migrate/20190208223837_create_users.rb
+++ db/migrate/20190208223837_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :pseudo, unique: true
-      t.string :email, unique: true
+      t.string :pseudo
+      t.string :email
       t.string :password_digest
       t.string :avatar_link
 
       t.timestamps
     end
+    add_index :users, :email, unique: true
+    add_index :users, :pseudo, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_02_08_223837) do
     t.string "avatar_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["pseudo"], name: "index_users_on_pseudo", unique: true
   end
 
 end
