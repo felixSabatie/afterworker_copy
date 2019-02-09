2c946fe4f50758585b7f91f5f7158511d17179f7
Unknown <felix.sabatie@gmail.com>
2019-02-09 13:53:26 -0500

    User validation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  validates :pseudo, presence: true, uniqueness: true
+
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+
+  validates :password, presence: true, length: {within: 6..40}
+
   has_secure_password
 end
