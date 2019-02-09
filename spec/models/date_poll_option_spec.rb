45afb2fc0bd58e54497ca776b2596bf82d7a26d6
Unknown <felix.sabatie@gmail.com>
2019-02-09 16:36:45 -0500

    Tests written

--- spec/models/date_poll_option_spec.rb
+++ spec/models/date_poll_option_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe DatePollOption, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should accept and save the date poll option' do
+    date_poll_option = build(:date_poll_option, event: create(:event))
+    expect(date_poll_option.save).to be true
+  end
+
+  context 'voters' do
+    it 'should add the users to the voters' do
+      user1 = create(:user)
+      user2 = create(:user)
+      date_poll_option = create(:date_poll_option, event: create(:event))
+      date_poll_option.voters << user1, user2
+
+      expect(date_poll_option.voters.length).to eql(2)
+    end
+
+    it 'should add the user only once to the voters' do
+      user1 = create(:user)
+      date_poll_option = create(:date_poll_option, event: create(:event))
+      date_poll_option.voters << user1
+      date_poll_option.voters << user1
+
+      expect(date_poll_option.voters.length).to eql(1)
+    end
+  end
 end
