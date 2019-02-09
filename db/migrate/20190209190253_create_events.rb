c82ecacce12548062167fc614d072a9d1d75a14c
Unknown <felix.sabatie@gmail.com>
2019-02-09 15:05:31 -0500

    Rename hash to event_hash

--- db/migrate/20190209190253_create_events.rb
+++ db/migrate/20190209190253_create_events.rb
@@ -4,14 +4,14 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :name
       t.boolean :is_open_to_dates
       t.boolean :is_open_to_places
-      t.string :hash
+      t.string :event_hash
       t.boolean :has_date_poll
       t.boolean :has_place_poll
       t.references :user, foreign_key: true
 
       t.timestamps
     end
-    add_index :events, :hash, unique: true
+    add_index :events, :event_hash, unique: true
 
     create_table :events_users, id: false do |t|
       t.belongs_to :event, index: true
