9a85553bdfe42cdeb2b06bb5081d3ad63eedc540
Unknown <felix.sabatie@gmail.com>
2019-02-10 12:08:58 -0500

    Specs done

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @event = create(:event, creator: create(:user))
+    @user = create(:user)
+  end
+
+  it 'should succesfully save the invite with a generated token' do
+    invite = build(:invite, user: @user, event: @event)
+    expect(invite.save).to be true
+    expect(invite.token).to be_truthy
+  end
+
+  context 'validation' do
+    it 'should refuse the invite because token already exists' do
+      invite = create(:invite, user: @user, event: @event)
+      invite2 = build(:invite, user: @user, event: @event)
+      invite2.token = invite.token
+      invite2.validate
+      expect(invite2.valid?).to be false
+      expect(invite2.errors.messages).to have_key(:token)
+    end
+  end
+
+  context 'relationships' do
+    it 'should add the invite to the user\'s invites' do
+      create(:invite, user: @user, event: @event)
+      expect(@user.invites.length).to eql(1)
+    end
+
+    it 'should add the invite to the event\'s invites' do
+      create(:invite, user: @user, event: @event)
+      expect(@event.invites.length).to eql(1)
+    end
+  end
 end
