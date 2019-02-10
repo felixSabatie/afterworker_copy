1f417615faa6b60c6c4ac16c4e4580c2cdd2143a
Unknown <felix.sabatie@gmail.com>
2019-02-10 15:08:27 -0500

    Generate knock stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    post 'user_token' => 'user_token#create'
+  end
+
   root to: 'static_files#index'
   get '*path', to: 'static_files#index'
 end
