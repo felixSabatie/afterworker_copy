a1eca9f025b8410e38de482e4ab9970de5c06014
Unknown <felix.sabatie@gmail.com>
2019-02-10 18:20:32 -0500

    Users controller create done

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,41 @@
 module Api
   class UsersController < ApplicationController
+    def create
+      user_params = params.require(:user).permit(:pseudo, :email, :password, :password_confirmation)
+      user = User.new(user_params)
+      already_exists = false
+      errors = []
+
+      if User.find_by(pseudo: user.pseudo) != nil
+        already_exists = true
+        errors << 'pseudo'
+      end
+      if User.find_by(email: user.email) != nil
+        already_exists = true
+        errors << 'email'
+      end
+
+      if already_exists
+        render status: 409, json: {errors: errors}
+      else
+        if user.save
+          render_json_with_includes(user)
+        else
+          render status: 422, json: user.errors.messages
+        end
+      end
+    end
+
+    private
+
+    def render_json_with_includes(user)
+      render json: {user: user}, include: [
+          {
+              events: {
+                  include: [:chosen_place, :chosen_date]
+              }
+          }], except: [:password_digest]
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     post 'user_token' => 'user_token#create'
+    resource :users, only: ['create']
   end
 
   root to: 'static_files#index'
