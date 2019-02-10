04340bb30846d1f9cf094f0cddad946d243dc351
Unknown <felix.sabatie@gmail.com>
2019-02-10 17:29:26 -0500

    Move user controller to api module

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe Api::UsersController, type: :controller do
   describe 'POST #create' do
     context 'valid params' do
       before do
