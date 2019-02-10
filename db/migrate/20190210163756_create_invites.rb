313619320381fc6b7e67927de32ce3c8b4684d3e
Unknown <felix.sabatie@gmail.com>
2019-02-10 11:38:36 -0500

    Generate invite model

---
+++ db/migrate/20190210163756_create_invites.rb
@@ -0,0 +1,11 @@
+class CreateInvites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :invites do |t|
+      t.string :token
+      t.references :event, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
