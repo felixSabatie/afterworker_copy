e6e66e7df08303d6e5a20706af7c5758c6ae2fa9
Unknown <felix.sabatie@gmail.com>
2019-02-11 17:59:38 -0500

    Reactive form working

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userInfos: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.buildForm();
+  }
 
   ngOnInit() {
+  }
+
+  buildForm() {
+    this.userInfos = this.fb.group({
+      email: [''],
+      password: [''],
+    });
+  }
+
+  submitForm() {
+    console.log(this.userInfos.value);
   }
 
 }
