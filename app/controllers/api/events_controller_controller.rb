ca867c14e0c4c7ba334d6e68279831aab9171644
Félix Sabatié <felix.sabatie@gmail.com>
2019-02-23 10:50:07 -0500

    Generated controller, tests done

---
+++ app/controllers/api/events_controller_controller.rb
@@ -0,0 +1,4 @@
+module Api
+  class EventsControllerController < ApplicationController
+  end
+end
--- spec/factories/date_poll_options.rb
+++ spec/factories/date_poll_options.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :date_poll_option do
     date { Time.now + 10.days }
     event { nil }
+
+    factory :date_poll_option_old do
+      date { Time.now - 10.days }
+    end
   end
 end
