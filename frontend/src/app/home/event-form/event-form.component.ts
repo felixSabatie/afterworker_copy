46ded61b10e04dd4ff347f6bf230c8f9810ad53d
Félix Sabatié <felix.sabatie@gmail.com>
2019-03-02 15:34:58 -0500

    Added date picker to view

--- frontend/src/app/home/event-form/event-form.component.ts
+++ frontend/src/app/home/event-form/event-form.component.ts
@@ -11,18 +11,21 @@ export class EventFormComponent implements OnInit {
   submitted = false;
   waitingForResponse = false;
   errors: string[] = [];
+  startAt = new Date();
 
   constructor(private fb: FormBuilder) {
+    this.startAt.setHours(this.startAt.getHours() + 1);
+    this.startAt.setMinutes(0);
+    this.startAt.setSeconds(0);
     this.eventInfos = this.fb.group({
       name: [''],
       has_date_poll: [true],
       has_place_poll: [true],
       is_open_to_dates: [true],
       is_open_to_places: [true],
-      place: ['']
+      place: [''],
+      date: [''],
     });
-
-    // TODO integrate https://www.npmjs.com/package/ng-pick-datetime
   }
 
   ngOnInit() {
