f2aa02864672db9686cc17e7c99172456d4ab0bf
Félix Sabatié <felix.sabatie@gmail.com>
2019-03-11 18:07:05 -0400

    POST create tests done

--- spec/controllers/api/place_poll_controller_spec.rb
+++ spec/controllers/api/place_poll_controller_spec.rb
@@ -4,50 +4,127 @@ RSpec.describe Api::PlacePollController, type: :controller do
 
   describe 'POST #create' do
     context 'valid params' do
-      it 'should create the place poll option' do
+      before do
+        user = create(:user)
+        user2 = create(:user)
+        event = create(:event, creator: user)
 
+        event.participants << user2
+        add_authenticated_header(request, user2)
+        post :create, params: {hash: event.event_hash,
+                               place_poll_option: attributes_for(:place_poll_option, event: event)}
+        @json = JSON.parse(response.body)
+      end
+
+      it 'should return code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should create the place poll option' do
+        expect(PlacePollOption.count).to eq(1)
       end
 
       it 'should return the place poll option' do
-
+        expect(@json['place_poll_option']['id']).to be_truthy
       end
     end
 
     context 'valid params and closed to suggestions' do
-      it 'should create the place poll option' do
+      before do
+        user = create(:user)
+        user2 = create(:user)
+        event = build(:event, creator: user)
+        event.is_open_to_places = false
+        event.save
 
+        event.participants << user2
+        add_authenticated_header(request, user)
+        post :create, params: {hash: event.event_hash,
+                               place_poll_option: attributes_for(:place_poll_option, event: event)}
+        @json = JSON.parse(response.body)
+      end
+
+      it 'should return code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should create the place poll option' do
+        expect(PlacePollOption.count).to eq(1)
       end
 
       it 'should return the place poll option' do
-
+        expect(@json['place_poll_option']['id']).to be_truthy
       end
     end
 
     context 'wrong params' do
-      it 'should return 422' do
-
-      end
-
       it 'should return 404 when the event does not exist' do
+        user = create(:user)
+        user2 = create(:user)
+        event = build(:event, creator: user)
+        event.is_open_to_places = false
+        event.save
 
+        event.participants << user2
+        add_authenticated_header(request, user)
+        post :create, params: {hash: 'wrong',
+                               place_poll_option: attributes_for(:place_poll_option, event: event)}
+
+        expect(response).to have_http_status(404)
       end
     end
 
     context 'unauthorized' do
       it 'should return 401 because the user is not logged in' do
+        user = create(:user)
+        user2 = create(:user)
+        event = create(:event, creator: user)
 
+        event.participants << user2
+        post :create, params: {hash: event.event_hash,
+                               place_poll_option: attributes_for(:place_poll_option, event: event)}
+
+        expect(response).to have_http_status(401)
       end
 
       it 'should return 401 because the user cannot access the event' do
+        user = create(:user)
+        user2 = create(:user)
+        event = create(:event, creator: user)
 
+        add_authenticated_header(request, user2)
+        post :create, params: {hash: event.event_hash,
+                               place_poll_option: attributes_for(:place_poll_option, event: event)}
+
+        expect(response).to have_http_status(401)
       end
 
       it 'should return 401 because the user cannot suggest places' do
+        user = create(:user)
+        user2 = create(:user)
+        event = build(:event, creator: user)
+        event.is_open_to_places = false
+        event.save
 
+        event.participants << user2
+        add_authenticated_header(request, user2)
+        post :create, params: {hash: event.event_hash,
+                               place_poll_option: attributes_for(:place_poll_option, event: event)}
+
+        expect(response).to have_http_status(401)
       end
 
       it 'should return 401 because the event does not have a place poll' do
+        user = create(:user)
+        event = build(:event, creator: user)
+        event.has_place_poll = false
+        event.save
 
+        add_authenticated_header(request, user)
+        post :create, params: {hash: event.event_hash,
+                               place_poll_option: attributes_for(:place_poll_option, event: event)}
+
+        expect(response).to have_http_status(401)
       end
     end
   end
@@ -79,7 +156,7 @@ RSpec.describe Api::PlacePollController, type: :controller do
       end
 
       it 'should return 401 because the user cannot access the event' do
-        
+
       end
 
       it 'should return 401 because the event does not have a place poll' do
