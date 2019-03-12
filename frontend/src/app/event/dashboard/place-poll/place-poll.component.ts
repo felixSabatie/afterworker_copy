f0624972ce21eb4387b1eaa8231680da9b3b4da2
Unknown <felix.sabatie@gmail.com>
2019-03-12 08:28:29 -0400

    Generated place poll component

---
+++ frontend/src/app/event/dashboard/place-poll/place-poll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-poll',
+  templateUrl: './place-poll.component.html',
+  styleUrls: ['./place-poll.component.scss']
+})
+export class PlacePollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
