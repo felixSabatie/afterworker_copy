749247de43783c585a13e0175258b618a463fecb
Félix Sabatié <felix.sabatie@gmail.com>
2019-03-12 15:33:04 -0400

    Date poll done

--- frontend/src/app/event/dashboard/date-poll/date-poll.component.ts
+++ frontend/src/app/event/dashboard/date-poll/date-poll.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, Input, LOCALE_ID, OnInit} from '@angular/core';
+import {Event} from "../../../models/event.model";
+import {User} from "../../../models/user.model";
+import {VotingItem} from "../../../models/voting-item.model";
+import {faCalendarAlt} from "@fortawesome/free-solid-svg-icons";
+import {DatePollService} from "./date-poll.service";
+import {formatDate} from "@angular/common";
+import {DatePollOption} from "../../../models/date-poll-option.model";
 
 @Component({
   selector: 'app-date-poll',
@@ -6,10 +13,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-poll.component.scss']
 })
 export class DatePollComponent implements OnInit {
+  @Input() event: Event;
+  @Input() currentUser: User;
 
-  constructor() { }
+  date: Date;
+  datePollVotingItems: VotingItem[] = [];
+  faCalendarAlt = faCalendarAlt;
+  waitingForResponse = false;
+  startAt = new Date();
+
+  constructor(private datePollService: DatePollService,
+              @Inject(LOCALE_ID) public locale: string) {
+    this.startAt.setHours(this.startAt.getHours() + 1);
+    this.startAt.setMinutes(0);
+    this.startAt.setSeconds(0);
+  }
 
   ngOnInit() {
+    this.datePollVotingItems = this.event.date_poll_options.map(datePollOption => {
+      return {
+        id: datePollOption.id,
+        name: this.dateToString(datePollOption.date),
+        voters: datePollOption.voters,
+      }
+    });
+    this.orderPlacePollVotingItems();
+  }
+
+  orderPlacePollVotingItems() {
+    this.datePollVotingItems.sort((item1, item2) => item2.voters.length - item1.voters.length);
+  }
+
+  changeVote(e: any) {
+    if (e.voted) {
+      this.datePollVotingItems.find(item => item.id === e.optionId).voters
+        .push(this.currentUser);
+    } else {
+      const voters = this.datePollVotingItems.find(item => item.id === e.optionId).voters;
+      const currentUserVoterId = voters.findIndex(voter => voter.id === this.currentUser.id);
+      voters.splice(currentUserVoterId, 1);
+    }
+
+    this.orderPlacePollVotingItems();
+    this.datePollService.toggleVote(this.event, e.optionId).subscribe();
+  }
+
+  createDate() {
+    if (this.date !== undefined) {
+      this.waitingForResponse = true;
+      this.datePollService.createDatePollOption(this.event, {date: this.date} as DatePollOption)
+        .subscribe(datePollOption => {
+          this.datePollVotingItems = [...this.datePollVotingItems, {
+            id: datePollOption.id,
+            name: this.dateToString(this.date),
+            voters: datePollOption.voters,
+          } as VotingItem];
+          this.date = undefined;
+          this.waitingForResponse = false;
+        });
+    }
+  }
+
+  dateToString(date: Date): string {
+    return formatDate(date, 'LLL dd yyyy, HH:mm', this.locale);
   }
 
 }
--- frontend/src/app/event/dashboard/date-poll/date-poll.service.ts
+++ frontend/src/app/event/dashboard/date-poll/date-poll.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Event} from "../../../models/event.model";
+import {Observable} from "rxjs";
+import {environment} from "../../../../environments/environment";
+import {map} from "rxjs/operators";
+import {DatePollOption} from "../../../models/date-poll-option.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatePollService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  createDatePollOption(event: Event, datePollOption: DatePollOption): Observable<DatePollOption> {
+    return this.http.post<any>(`${environment.baseUrl}/events/${event.event_hash}/date-poll`,
+      {date_poll_option: datePollOption})
+      .pipe(map(response => response.date_poll_option));
+  }
+
+  toggleVote(event: Event, datePollOptionId: number): Observable<any> {
+    return this.http.post(`${environment.baseUrl}/events/${event.event_hash}/date-poll/${datePollOptionId}/toggle`, {})
+  }
 }
