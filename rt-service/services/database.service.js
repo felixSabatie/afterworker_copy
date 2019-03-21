084358045b7b3da48ee89bb879f202448c59ad22
Félix Sabatié <felix.sabatie@gmail.com>
2019-03-21 16:43:41 -0400

    Heroku ready ?

--- rt-service/services/database.service.js
+++ rt-service/services/database.service.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 
 class DatabaseService {
     static connect(callback) {
-        MongoClient.connect('mongodb://afterworker:afterworker@mongodb:27017/afterworker?authSource=admin', (err, client) => {
+        MongoClient.connect(process.env.MONGODB_URI || 'mongodb://afterworker:afterworker@mongodb:27017/afterworker?authSource=admin', (err, client) => {
             if (err) throw err;
             callback(client.db('afterworker'));
         });
