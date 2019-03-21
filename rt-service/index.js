21bd27d7d8d41d7db7a22bc45fe1553e1a028edc
Félix Sabatié <felix.sabatie@gmail.com>
2019-03-21 16:22:15 -0400

    Connection to mongodb working

--- rt-service/index.js
+++ rt-service/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const DatabaseService = require('./services/database.service');
 
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.json('Hello world');
+DatabaseService.connect(db => {
+    app.get('/', (req, res) => {
+        // db.collection('messages').insert({userId: 2, message: 'Test message'}, (err, records) => {
+        //     if (err) return res.json({error: err});
+            db.collection('messages').find().toArray((err, result) => {
+                res.json({messages: result});
+            });
+        // });
+    })
 });
 
 app.listen(port);
--- rt-service/services/database.service.js
+++ rt-service/services/database.service.js
@@ -2,9 +2,9 @@ const MongoClient = require('mongodb').MongoClient
 
 class DatabaseService {
     static connect(callback) {
-        MongoClient.connect('mongodb://localhost:27017/afterworker', (err, db) => {
+        MongoClient.connect('mongodb://afterworker:afterworker@mongodb:27017/afterworker?authSource=admin', (err, client) => {
             if (err) throw err;
-            callback(db);
+            callback(client.db('afterworker'));
         });
     }
 }
