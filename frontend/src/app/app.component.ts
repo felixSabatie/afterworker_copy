974dfbe9a6166104eba1c3bd76f8129346c71d65
Unknown <felix.sabatie@gmail.com>
2019-03-26 09:58:48 -0400

    Prompt refresh on app update available

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private swUpdate: SwUpdate) { }
+
+  ngOnInit() {
+    this.swUpdate.available.subscribe(event => {
+      if (confirm('A new update is available, do you want to refresh now ?')) {
+        window.location.reload();
+      }
+    });
+
+    this.swUpdate.checkForUpdate();
+  }
 }
